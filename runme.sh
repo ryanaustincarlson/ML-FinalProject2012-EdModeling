547ca1493c30c4eafcfc039a5f5cfd8e54e5600d
Ryan Carlson <rcarlso1@gmail.com>
2012-10-24 16:52:30 -0400

    made runme script work from root directory

--- runme.sh
+++ runme.sh
@@ -3,4 +3,6 @@
 # examples of commands to run
 # run this script (from the terminal, type ./runme.sh) to run each of these in sequence
 
-python extract-logdata.py data/alldata.csv output/processed.csv
+cd extraction-and-formatting
+python extract-logdata.py ../data/alldata.csv ../output/processed.csv
+cd ..
