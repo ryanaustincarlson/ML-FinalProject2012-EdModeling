d29364bd200b10e82bda10305c8071cfe8b0c290
Konstantin Genin <konstantin.genin@gmail.com>
2012-11-01 14:51:46 -0400

    Latest studentPerformed -- trying to fix data loading problems.

--- extraction-and-formatting/processHints.R
+++ extraction-and-formatting/processHints.R
@@ -9,12 +9,18 @@ d <- read.csv(paste(dataPath, "studentPerformed.csv", sep=""))
 #create HINT flag
 d$hint<-0
 d$hint[d$Student.Response.Type == "HINT_REQUEST"] <- 1
+#summary(d$Student.Response.Type)
+#nrow(d[ which(d$hint==1),])
+#nrow(d[ which(d$hint==0),])
 
-summary(d$Student.Response.Type)
-nrow(d[ which(d$hint==1),])
-nrow(d[ which(d$hint==0),])
+#flag InCorrect Answers
+d$errror<-0
+d$error[d$Outcome == "InCorrect"] <- 1 
 
-#hints requested by student for each problem
-x<-ddply(d, .(Anon.Student.Id,Problem.Name), summarise, hints_req=sum(hint))
+#number of hints per student/problem
+#number of errors per student/problem
+x<-ddply(d, .(Anon.Student.Id,Problem.Name), summarise, hints_req=sum(hint), num_errors=sum(error)) 
+
+#grep("[a-z]",d[1:100,"Input"], value=TRUE)
 
 #nrow(d[which(d$Anon.Student.Id=="Stu_fb73356c923072b0b3a3bae4d71a327a" & d$Problem.Name==28&d$Student.Response.Type=="HINT_REQUEST"),])
--- studentPerformed.R
+++ studentPerformed.R
@@ -6,7 +6,7 @@ data1 <- read.csv(paste(dataPath, "original-data/data_students1-5.csv", sep=""))
 data2 <- read.csv(paste(dataPath, "original-data/data_students6-9.csv", sep=""))
 
 #make names consistent, append
-#data2 <- data2[c(1:15, 17, 16, 18:42)]
+data2 <- data2[c(1:15, 17, 16, 18:42)]
 names(data2)[names(data2)=="day"] <- "Day" 
 alldata <- rbind(data1, data2)
 
@@ -20,12 +20,6 @@ remove(data1, data2, alldata)
 studentPerformed <- subset(studentPerformed, select = -c(1,2,8:12, 14, 16, 17, 19, 25, 29:31, 33:38, 41:42))
 
 #write student-performed data
-write.table(studentPerformed, paste(dataPath, "studentPerformed.csv", sep=""))
-
-
-
-
-
-
+write.csv(studentPerformed, paste(dataPath, "studentPerformed.csv", sep=""))
 
 
