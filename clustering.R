f73b20ef13c8933c4ea269c0aae6ca2b7f72ff2b
Ryan Carlson <rcarlso1@gmail.com>
2012-11-27 21:50:10 -0500

    assign the most probable class to each student

--- clustering.R
+++ clustering.R
@@ -7,6 +7,57 @@ dataPath <- "data/"
 #load, sort data
 students <- read.csv(paste(dataPath, "students-nominalized.csv", sep=""))
 
+
+head(students)
+
+LCAstats <- function(students, nclasses)
+  # nclasses is a range, i.e. 1:4
+{
+  stats <- data.frame()
+  f <- cbind(hints_req, num_errors, minSpent, Inc..Cor, Inc..Hint, Inc..Inc, NumBOH) ~ 1 
+  for(nclass in nclasses) {
+    lc <- poLCA(f,students, nclass=nclass)
+    stats <- rbind(stats, data.frame(aic=lc$aic, bic=lc$bic, Gsq=lc$Gsq, Chisq=lc$Chisq))
+  }
+  stats
+}
+
+LCA <- function(students, nclass)
+{
+  f <- cbind(hints_req, num_errors, minSpent, Inc..Cor, Inc..Hint, Inc..Inc, NumBOH) ~ 1 
+  poLCA(f, students, nclass=nclass)
+}
+
+allstats <- LCAstats(students, 1:10)
+allstats
+
+
+chooseBestLatentClass <- function(student, lc, nclasses)
+{
+  features <- names(lc$probs)
+  classprobs <- rep(1, nclasses)
+  for (feature in features)
+  {
+    latent_distribution <- as.data.frame(lc$probs[[feature]])
+    student_value <- student[[feature]]
+    
+    prob <- latent_distribution[[paste("Pr(",student_value,")", sep="")]]
+    classprobs <- classprobs * prob
+    
+    #print(latent_distribution)
+    #print(student_value)
+    #print(prob)
+    #print(classprobs)
+    #print("-")
+  }
+  which.max(classprobs)
+}
+
+lc <- LCA(students, 4)
+students$latent <- by(students, 1:nrow(students), chooseBestLatentClass, lc, 4)
+
+
+" # my multiline comment..
 x <- cbind(matrix(students$hints_req), 
            matrix(students$num_errors),
            matrix(students$minSpent),
@@ -15,24 +66,6 @@ x <- cbind(matrix(students$hints_req),
            matrix(students$Inc..Inc),
            matrix(students$NumBOH))
 
-head(students)
-
-LCAstats <- function(nclasses)
-  # nclasses is a range, i.e. 1:4
-{
-  stats <- data.frame()
-  for(nclass in nclasses) {
-    f <- cbind(hints_req, num_errors, minSpent, Inc..Cor, Inc..Hint, Inc..Inc, NumBOH) ~ 1 
-    lc <- poLCA(f,students, nclass=nclass)
-    stats <- rbind(stats, data.frame(aic=lc$aic, bic=lc$bic, Gsq=lc$Gsq, Chisq=lc$Chisq))
-  }
-  stats
-}
-
-allstats <- LCAstats(1:10)
-allstats
-
-" # my multiline comment..
 # Model Based Clustering
 #fit <- Mclust(x)
 #plot(fit, x) # plot results 
