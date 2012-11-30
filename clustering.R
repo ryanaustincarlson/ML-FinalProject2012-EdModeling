8445484bce1f923c4dd0a7b1ab235d52c01957f3
Ryan Carlson <rcarlso1@gmail.com>
2012-11-29 22:15:51 -0500

    updated clustering.R

--- clustering.R
+++ clustering.R
@@ -7,30 +7,81 @@ dataPath <- "data/"
 #load, sort data
 students <- read.csv(paste(dataPath, "students-nominalized.csv", sep=""))
 
+MAXITER <- 5000
+
+#LCAfeatures <- cbind(hints_req, num_errors, minSpent, Inc..Cor, Inc..Hint, Inc..Inc, NumBOH) ~ 1 
+LCAfeatures <- cbind(
+  hints_req, 
+  num_errors, 
+  minSpent 
+  #deltaErrors, 
+  #intrcpErrors, 
+  #deltaHints, 
+  #intrcpHints, 
+  #Inc..Cor, 
+  #Inc..Hint, 
+  #Inc..Inc, 
+  #NumBOH, 
+  #firstHintGeom, 
+  #hintsGeom, 
+  #errorsGeom, 
+  #stubbornGeom,
+  #numErrors1, 
+  #numErrors2, 
+  #numErrors3, 
+  #numErrors4, 
+  #numHints1, 
+  #numHints2, 
+  #numHints3, 
+  #numHints4,
+  #steptime1, 
+  #steptime2, 
+  #steptime3, 
+  #steptime4, 
+  #firstHint1, 
+  #firstHint2, 
+  #firstHint3, 
+  #firstHint4
+  ) ~ 1 
 
 head(students)
+
+
+LCA <- function(students, nclass)
+{
+  poLCA(LCAfeatures, students, nclass=nclass, maxiter=MAXITER)
+}
 
 LCAstats <- function(students, nclasses)
   # nclasses is a range, i.e. 1:4
 {
   stats <- data.frame()
-  f <- cbind(hints_req, num_errors, minSpent, Inc..Cor, Inc..Hint, Inc..Inc, NumBOH) ~ 1 
+
   for(nclass in nclasses) {
-    lc <- poLCA(f,students, nclass=nclass)
-    stats <- rbind(stats, data.frame(aic=lc$aic, bic=lc$bic, Gsq=lc$Gsq, Chisq=lc$Chisq))
+    lc <- LCA(students, nclass)
+    stats <- rbind(stats, data.frame(aic=lc$aic, bic=lc$bic, 
+                                     Gsq=lc$Gsq, Chisq=lc$Chisq, 
+                                     params=lc$npar, degfree=lc$resid.df,
+                                     loglik=lc$llik))
   }
   stats
-}
-
-LCA <- function(students, nclass)
-{
-  f <- cbind(hints_req, num_errors, minSpent, Inc..Cor, Inc..Hint, Inc..Inc, NumBOH) ~ 1 
-  poLCA(f, students, nclass=nclass)
 }
 
 allstats <- LCAstats(students, 1:10)
 allstats
 
+plotStats <- function(allstats)
+{
+  
+  x1 <- 1:length(allstats$aic)
+  y1 <- allstats$aic
+  x2 <- 1:length(allstats$bic)
+  y2 <- allstats$bic
+  
+  plot(x1, y1, col="green", ylab="", xlab="# Latent Classes", xlim=range(x1,x2), ylim=range(y1,y2))
+  points(x2,y2, col="red")
+}
+plotStats(allstats)
 
 chooseBestLatentClass <- function(student, lc, nclasses)
 {
@@ -53,37 +104,25 @@ chooseBestLatentClass <- function(student, lc, nclasses)
   which.max(classprobs)
 }
 
-lc <- LCA(students, 4)
-students$latent <- by(students, 1:nrow(students), chooseBestLatentClass, lc, 4)
+pickBestLCA <- function(students, nclass)
+{
+  bestLCA <- NULL
+  for (iter in 1:100)
+  {
+    lc <- LCA(students, nclass)
+    if (is.null(bestLCA)) { 
+      bestLCA <- lc 
+    }
+    if (lc$llik > bestLCA$llik) { 
+      bestLCA <- lc
+      print(paste("changing LCA from",bestLCA$llik,"to",lc$llik))
+    }
+  }
+  bestLCA
+}
 
-
-" # my multiline comment..
-x <- cbind(matrix(students$hints_req), 
-           matrix(students$num_errors),
-           matrix(students$minSpent),
-           matrix(students$Inc..Cor),
-           matrix(students$Inc..Hint),
-           matrix(students$Inc..Inc),
-           matrix(students$NumBOH))
-
-# Model Based Clustering
-#fit <- Mclust(x)
-#plot(fit, x) # plot results 
-#print(fit) # display the best model
-
-# Ward Hierarchical Clustering
-d <- dist(x, method = "euclidean") # distance matrix
-fit <- hclust(x)#, method="ward") 
-plot(fit) # display dendogram
-groups <- cutree(fit, k=5) # cut tree into 5 clusters
-# draw dendogram with red borders around the 5 clusters 
-rect.hclust(fit, k=5, border="red")
-
-# # a 2-dimensional example
-# x <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 2),
-#            matrix(rnorm(100, mean = 1, sd = 0.3), ncol = 2))
-# colnames(x) <- c("x", "y")
-# (cl <- kmeans(x, 2))
-# plot(x, col = cl$cluster)
-# points(cl$centers, col = 1:2, pch = 8, cex=2)
-"
+nBestClass <- 2
+lc <- pickBestLCA(students, nBestClass)
+#lc <- LCA(students, nBestClass)
+students$latent <- by(students, 1:nrow(students), chooseBestLatentClass, lc, nBestClass)
+chisq.test(table(students$latent,students$Condition))
