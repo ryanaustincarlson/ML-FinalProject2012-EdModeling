3da56c4ab7c6f54c175be3e50c095d12bbc373dd
Ryan Carlson <rcarlso1@gmail.com>
2012-11-29 22:16:20 -0500

    moved student nominalization data to splitStudentData

--- splitStudentData.R
+++ splitStudentData.R
@@ -1,12 +1,21 @@
 dataPath <- "data/"
-students <- read.csv(paste(dataPath, "students.csv", sep=""))
+filename <- "allFeatures.csv"
+students <- read.csv(paste(dataPath, filename, sep=""))
 
-featurenames <- names(students)[4:length(names(students))]
+featurenames <- names(students)[5:length(names(students))]
 
 for (featurename in featurenames)
 {
-  print(paste("feature name:", featurename))
   featurevals <- students[[featurename]]
-  splits <- quantile(featurevals, seq(0,1,len=6))
-  print(splits)
+  splits <- unique(quantile(featurevals, seq(0,1,len=6), na.rm=TRUE))
+  categories <- cut(featurevals, splits, labels=FALSE, include.lowest=TRUE)
+  students[[featurename]] <- categories
 }
+
+for (featurename in featurenames)
+{
+  featurevals <- students[[featurename]]
+  print(unique(featurevals))
+}
+
+write.csv(students, paste(dataPath, "students-nominalized.csv", sep=""))
